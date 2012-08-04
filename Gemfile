f149e1715f1187ad4f82f51e530d1201554e3b0d
Nucc <nucc@bteam.hu>
2012-08-04 12:10:41 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# Using http because ruby-1.9.3 https library throws segfault
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.7'
+gem 'sqlite3', '1.3.6'
+gem 'jquery-rails', '2.0.2'
+gem 'rspec-rails', '2.11.0'
+
+
+group :development do
+	gem 'debugger'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkingTool::Application.config.secret_token = 'c18d3809ced3db95f6cf849650a3e63f0dbf4ed4173714224a041bb8e40dc09cf8e746c2f0f798d7efa2fbe096e1ff15a62a982bd98538e4712a98ca87136bb1'
