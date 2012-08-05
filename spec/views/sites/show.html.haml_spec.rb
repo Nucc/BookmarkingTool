27d9011fa4010be56c6e9df94278ea18b3d84263
Nucc <nucc@bteam.hu>
2012-08-05 19:21:01 +0200

    Use collector and tinyurl mocks in sites/show spec

--- spec/views/sites/show.html.haml_spec.rb
+++ spec/views/sites/show.html.haml_spec.rb
@@ -2,13 +2,27 @@ require 'spec_helper'
 
 describe "sites/show" do
 
+    before :each do
+        @collector = BookingToolMocks::MetaInfo.new
+        @collector.title = "Title"
+        @collector.description = "Description"
+
+        @shortener = BookingToolMocks::TinyURL.new
+        @shortener.body = "http://tinyurl.com/alpha"
+    end
+
     it "is able to list bookmarks of a site" do
         site = stub_model(Site, :id => 1, :domain => "alphasights.com")
 
-        bookmarks = [
-            stub_model(Bookmark, :id => 1, :site_id => 1, :url => "alphasights.com/careers"),
-            stub_model(Bookmark, :id => 2, :site_id => 1, :url => "alphasights.com/what-we-do")
-        ]
+
+        @collector.title = "CAREERS"
+        bookmarks = []
+        bookmarks << stub_model(Bookmark, :id => 1, :meta_collector => @collector, :url_shortener => @shortener,
+                                 :site_id => 1, :url => "alphasights.com/careers")
+
+        @collector.title = "WHATWEDO"
+        bookmarks << stub_model(Bookmark, :id => 2, :meta_collector => @collector, :url_shortener => @shortener,
+                                 :site_id => 1, :url => "alphasights.com/what-we-do")
 
         site.stub!(:bookmarks).and_return(bookmarks)
         assign :site, site
@@ -16,8 +30,8 @@ describe "sites/show" do
         render
 
         rendered.should have_xpath("//div[@id='site']//*[text()='alphasights.com']")
-        rendered.should have_xpath("//div[@id='site']//li//a[@href='/bookmarks/1' and text()='AlphaSights']")
-        rendered.should have_xpath("//div[@id='site']//li//a[@href='/bookmarks/2' and text()='http://alphasights.com/what-we-do']")
+        rendered.should have_xpath("//div[@id='site']//li//a[@href='/bookmarks/1' and text()='CAREERS']")
+        rendered.should have_xpath("//div[@id='site']//li//a[@href='/bookmarks/2' and text()='WHATWEDO']")
     end
 
     it "displays No site has been found when site is missing" do
