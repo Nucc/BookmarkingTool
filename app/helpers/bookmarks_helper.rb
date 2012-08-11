66b30cbbaa765e46b9d5aa443f680f10fa0ef478
Nucc <nucc@bteam.hu>
2012-08-11 20:08:53 +0200

    Use title.presence in bookmark helper

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,11 +1,11 @@
 module BookmarksHelper
 
     def title_of(bookmark)
-        (bookmark.title and bookmark.title.length > 0) ? bookmark.title : "No title"
+        bookmark.title.presence || "No title"
     end
 
     def description_of(bookmark)
-        bookmark.description ? bookmark.description : "No description"
+        bookmark.description || "No description"
     end
 
 end
